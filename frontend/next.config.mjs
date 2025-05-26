e0e48c72e7828173ded3f37f8880c1d943d61eb9
Anjal Joshi <133899213+notcool100@users.noreply.github.com>
2025-05-26 15:17:57 +0545

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 Required for static export (next export)
+
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,7 +9,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // 👈 Required for static export if using <Image>
   },
 }
 
